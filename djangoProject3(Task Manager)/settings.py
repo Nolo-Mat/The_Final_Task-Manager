91eacd231ee99bbf7db1b7bd54471ae405fbf0e7
Lehlohonolo_Matlala <120008049+Nolo-Mat@users.noreply.github.com>
2023-04-29 19:09:50 +0200

    Update settings.py

--- djangoProject3(Task Manager)/settings.py
+++ djangoProject3(Task Manager)/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v(ogbgo%p2taxut$0fs99^lr^hs47fdj_4=loc(db=n(j97sra'
+SECRET_KEY = '*****************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
